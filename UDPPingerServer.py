ecabc6fc2efb1dfbffca4f68db60efb9d06ad253
Rehas Sachdeva <aquannie@gmail.com>
2016-03-31 19:31:56 +0530

    Modified Server to run at arbitrary port

--- UDPPingerServer.py
+++ UDPPingerServer.py
@@ -1,10 +1,18 @@
 import random
+import sys
 from socket import *
+
+# Check command line arguments
+if len(sys.argv) != 2:
+    print "Usage: python UDPPingerServer <server port no>"
+    sys.exit()
+
 # Create a UDP socket
 # Notice the use of SOCK_DGRAM for UDP packets
 serverSocket = socket(AF_INET, SOCK_DGRAM)
 # Assign IP address and port number to socket
-serverSocket.bind(('', 12000))
+serverSocket.bind(('', int(sys.argv[1])))
+
 while True:
 	# Generate random number in the range of 0 to 10
 	rand = random.randint(0, 10)
